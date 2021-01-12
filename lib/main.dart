c0f5909074e5d5def69d6c21478b18b99f0e8c60
francisco <frmauro8@hotmail.com>
2021-01-12 15:36:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:product_app/ui/home_page.dart';
+
+void main() async {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+
+const urlApi = "http://192.168.15.32:8088/";
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
