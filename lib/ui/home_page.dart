4810b980defabe14225477fcc3cf0a0b85cf0faf
francisco <frmauro8@hotmail.com>
2021-01-12 17:00:45 -0300

    create home_page

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
 
+import 'package:product_app/model/product.dart';
+
 const urlApi = "http://localhost:8087/product/";
 
 class Home extends StatefulWidget {
@@ -11,8 +13,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final _productController = TextEditingController();
+  List<Product> _products = [];
+
+  _setHeaders() => {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Products"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: _products.length,
+        itemBuilder: (context, index) {
+          return null;
+        },
+      ),
+    );
   }
 }
